e60b32be205e61ed292214b7a7bc4161c0c89f1e
Symon <nirmalbdrbk@gmail.com>
2026-01-28 18:04:33 +0545

    learn inputs in python

---
+++ prac.py
@@ -0,0 +1,41 @@
+age = 19
+price =1222
+
+price = age
+print(age)
+
+
+# write 2 numbers and print the sum of them 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("The sum of two numbers is:", sum)
+
+
+# WAP to input sides of square and print its area
+side = int(input("Enter the sides of square: "))
+area = side * side
+print("The area of square is:", area)
+
+
+
+# WAP to input 2 floating point numbers and print their average
+a = float(input("Enter first floating point number: "))
+b = float(input("Enter second floating point number: "))
+average = (a + b) / 2
+print("The average of two floating point numbers is:", average)
+
+
+
+# WAP to input 2 integers, a,b and print true if a is greater than equal to b else print false
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+if a >= b:
+    print("True")
+else:
+    print("False")
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+
+print(a >= b)
