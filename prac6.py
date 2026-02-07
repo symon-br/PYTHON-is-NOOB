380e37564112660d5fda90caef859ace8a991c52
Symon <nirmalbdrbk@gmail.com>
2026-02-07 23:22:54 +0545

    LEARNING FUNCTIONS

---
+++ prac6.py
@@ -0,0 +1,16 @@
+#Functions definition
+def calc_Sum(a, b): #Parameters
+    return a + b
+
+sum = calc_Sum(1, 4) #Function call with Arguments
+print(sum)
+
+# Function is made to reduce the code redundancy and to make the code more modular and reusable.
+
+
+def No_params():
+    print(" ") #Free space; can write anything here.
+
+y= No_params() #Function call without arguments 
+print(y) #This will print None because the function does not return anything.
+
