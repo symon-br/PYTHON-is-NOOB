63db21008830c9da915d463c000de5c7b2b01398
Symon <nirmalbdrbk@gmail.com>
2026-02-07 23:25:03 +0545

    Average of 3 numbers with function

--- prac6.py
+++ prac6.py
@@ -14,3 +14,17 @@ def No_params():
 y= No_params() #Function call without arguments 
 print(y) #This will print None because the function does not return anything.
 
+
+# Question 1
+
+def ave(a, b, c): 
+    sum = a + b + c
+    average = sum / 3
+    print(average) #This will print the average but the function will not return anything, so it will return None by default. If we want to return the average, we should use return statement instead of print statement.
+    return average #Only return; no print 
+
+# print(ave(1, 2, 3)) #IN decimal points
+# print(int(ave(1, 2, 3))) #IN whole number
+
+ave(1, 1, 1) #Function call
+
