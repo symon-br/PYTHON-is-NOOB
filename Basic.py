335735e2646fe4a089c8dd3ceaa153f109548542
Symon <nirmalbdrbk@gmail.com>
2026-02-15 08:12:03 +0545

    PYTHON codes

---
+++ Basic.py
@@ -0,0 +1,28 @@
+# def cal_area_of_rectangle(l, b):
+#         data = l * b 
+#         print(data)
+
+# cal_area_of_rectangle(4,2) #function call
+
+
+
+#1.	Write a for loop to print numbers from 1 to 10.
+# for a in range(1,11):
+#         print(a)
+        
+
+
+
+#2.	Use a while loop to print even numbers up to 50.
+# n = 0
+# while n <= 50:
+#       print(n)
+#       n+=2
+
+
+
+
+#3.	Write a loop to calculate the sum of numbers in a list.
+#4.	Print each character of a string using a loop.
+#5.	Write a loop to count how many elements are in a list.
+
